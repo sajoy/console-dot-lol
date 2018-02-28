d71ef75fa7df50245166095d9cac1ae50d962713
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-02-28 15:48:38 -0800

    allow block and rainbow to manage multiple arguments

--- console.js
+++ console.js
@@ -11,54 +11,80 @@ console.lol = () => {
     console.log(`%c${emojis.random()}`, 'font-size: 2rem;');
 };
 
-
 console.rainbow = function () {
+    const rainbowify = {
+        string: (arg) => {
+            const rainbowArg = colorString(arg);
+            const r = 'color: red;';
+            const o = 'color: orange;';
+            const y = 'color: yellow;';
+            const g = 'color: green;';
+            const b = 'color: blue;';
+            const i = 'color: indigo;';
+            const p = 'color: purple;';
+        
+            console.log(`☁️%c${rainbowArg}☁️`, r, o, y, g, b, i, p, i, b, g, y, o, r);
 
-    const args = [...arguments];
-    for (let arg of args) {
-
-        if (typeof arg !== 'string' && Array.isArray(arg)) arg = `[${arg.join()}]`;
-        if (typeof arg === 'object') {
+            function colorString (str) {
+                const txtLength = str.length;
+                const numColors = 13;
+                const charsPerColor  = Math.round(txtLength / numColors);
+        
+                const txtArray = [];
+                let startingI = 0, endingI = charsPerColor;
+                while (txtArray.length < 13) {
+                    txtArray.push(str.substr(startingI,endingI));
+                    startingI = txtArray.length * charsPerColor;
+                    if (txtArray.length === 12 && startingI < str.length) endingI = str.length;
+                }
+        
+                const rainbowStr = txtArray.join('%c');
+                return rainbowStr;
+            }
+        },
+        object: (arg) => {
             console.log(`🌈️`,arg);
-            continue;
         }
-
-        const rainbowArg = colorString(arg);
-        const r = 'color: red;';
-        const o = 'color: orange;';
-        const y = 'color: yellow;';
-        const g = 'color: green;';
-        const b = 'color: blue;';
-        const i = 'color: indigo;';
-        const p = 'color: purple;';
-
-        console.log(`☁️%c${rainbowArg}☁️`, r, o, y, g, b, i, p, i, b, g, y, o, r);
     };
 
-    function colorString (str) {
-        const txtLength = str.length;
-        const numColors = 13;
-        const charsPerColor  = Math.round(txtLength / numColors);
+    manageArgs(arguments, rainbowify);
+};
 
-        const txtArray = [];
-        let startingI = 0, endingI = charsPerColor;
-        while (txtArray.length < 13) {
-            txtArray.push(str.substr(startingI,endingI));
-            startingI = txtArray.length * charsPerColor;
-            if (txtArray.length === 12 && startingI < str.length) endingI = str.length;
+
+console.block = function () {
+    const blockify = {
+        string: (txt) => {
+            const styles = 'background-color: black; color: white; padding: 2px 15px; font-weight: bold; text-transform: uppercase;';
+            console.log(`%c------------- ${txt} -------------`, styles);
+        },
+        object: (txt) => {
+            console.log('◾⬛', txt, '⬛◾');
+        }
+    };
+
+    manageArgs(arguments, blockify);
+};
+
+
+function manageArgs (givenArguments, callbacks) {
+    let argumentsArray = [...givenArguments];
+    if (argumentsArray.length > 1) console.group('' + argumentsArray[0]);
+    for (let arg of argumentsArray) {
+        
+        if (typeof arg === 'string') callbacks.string(arg);
+        
+        if (Array.isArray(arg)) { 
+            arg = `[${arg.join()}]`;
+            callbacks.string(arg);
+        }
+        
+        if (typeof arg === 'object') {
+            callbacks.object(arg);
         }
 
-        const rainbowStr = txtArray.join('%c');
-        return rainbowStr;
     }
-};
-
-
-console.block = txt => {
-    const styles = 'background-color: black; color: white; padding: 2px 15px; font-weight: bold; text-transform: uppercase;';
-    console.log(`%c-------------${txt}-------------`, styles);
-};
-
+    if (argumentsArray.length > 1) console.groupEnd();
+}
 
 
 /*
--- tests.js
+++ tests.js
@@ -3,7 +3,8 @@ console.lol('This message was printed with console.lol');
 console.rainbow('This message was printed with console.rainbow');
 console.rainbow([1,2,4,'testing an array!']);
 console.rainbow('testing', 'multiple rainbow args', [1,2,3,4]);
-console.rainbow({lol:'pls',ok:'naj'}, 'prints obje cts')
+console.rainbow('prints obje cts', {lol:'pls',ok:'naj'})
 
 
 console.block('This message was printed with console.block');
+console.block('prints obje cts', {lol:'pls',ok:'naj'})
